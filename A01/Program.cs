3ffcc7759cef8cd4f75fdb6cbe946ee01d8c792c
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-07-28 18:45:00 +0530

    Implementation of simple guessing game program

---
+++ A01/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace A01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        Console.WriteLine("Think of a number between 1 and 100.");
+        int secretNumber = random.Next(1, 101), guess;
+        Console.WriteLine("Guess the number between 1 and 100...!");
+        do
+        {
+            Console.Write("Enter your guess: ");
+            if (!int.TryParse(Console.ReadLine(), out guess))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+            if (guess > secretNumber)
+            {
+                Console.WriteLine("Your guess is too high");
+            }
+            else if (guess < secretNumber)
+            {
+                Console.WriteLine("Your guess is too low");
+            }
+            else
+            {
+                Console.WriteLine("You guessed correctly");
+            }
+        } while (guess != secretNumber);
+
+    }
+}
