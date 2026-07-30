5e228d46c9aa9abcb24101b3d36e56554c6cf684
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-07-30 12:00:49 +0530

    Fix formatting as per metamation standard and update the editconfig file

--- A01/Program.cs
+++ A01/Program.cs
@@ -1,34 +1,25 @@
 ﻿namespace A01;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Random random = new Random();
-        Console.WriteLine("Think of a number between 1 and 100.");
-        var (secretNumber, attempts) = ((random.Next(1, 101), (0)));
+class Program {
+    static void Main (string[] args) {
+        Random random = new Random ();
+        Console.WriteLine ("Think of a number between 1 and 100.");
+        var (secretNumber, attempts) = ((random.Next (1, 101), (0)));
         const int maxAttempts = 7;
-        Console.WriteLine("Guess the number between 1 and 100...!");
-        while (attempts < maxAttempts)
-        {
-            Console.Write("Enter your guess: ");
-            int guess = int.Parse(Console.ReadLine());
+        Console.WriteLine ("Guess the number between 1 and 100...!");
+        while (attempts < maxAttempts) {
+            Console.Write ("Enter your guess: ");
+            int guess = int.Parse (Console.ReadLine ());
             attempts++;
-            if (guess == secretNumber)
-            {
-                Console.WriteLine("You guessed correctly!");
+            if (guess == secretNumber) {
+                Console.WriteLine ("You guessed correctly!");
                 return;
-            }
-            else if (guess > secretNumber)
-            {
-                Console.WriteLine("Your guess is too high");
-            }
-            else
-            {
-                Console.WriteLine("Your guess is too low");
+            } else if (guess > secretNumber) {
+                Console.WriteLine ("Your guess is too high");
+            } else {
+                Console.WriteLine ("Your guess is too low");
             }
         }
-        Console.WriteLine($"You used all {maxAttempts} guesses. The correct number was {secretNumber}.");
-
+        Console.WriteLine ($"You used all {maxAttempts} guesses. The correct number was {secretNumber}.");
     }
 }
