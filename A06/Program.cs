7a73f51f4f16ab5f8cb2a9ffd4a6cdabc7115a2b
SuvarajMetamation <Suvaraj.Balakrishnan@trumpf.com>
2026-08-17 16:18:01 +0530

    Initial setup for 8-Queens problems

---
+++ A06/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------
+// Program.cs
+// A06: Program to print unique solutions for the 8-Queens problem.
+// --------------------------------------------------------------------------------------------
+namespace A06;
+
+using static Console;
+
+class Program
+{
+    static void Main()
+    {
+      WriteLine ("Hello world");
+    }
+}
